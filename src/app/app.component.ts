ef489c126abf20c23dbe714de227e9a62cad9c3f
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-07-16 17:02:29 +0530

    redirecting pages completed by type script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router:Router) {
+
+  }
+  navigateToPage(link:string):void{
+    this.router.navigateByUrl(link)
+      .then(e=>{
+        console.log(e);
+      }).catch(error=>{
+      console.log(error);
+    })
+
+  }
   title = 'app3';
 }
