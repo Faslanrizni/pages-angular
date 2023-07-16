806f5decb04df2b704a331203a63c310c4a41428
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-07-16 09:22:21 +0530

    redirecting pages completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./form/home.component";
+import {CustomerComponent} from "./customer/customer.component";
+import {OrderComponent} from "./order/order.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'customer',component:CustomerComponent},
+  {path:'orders',component:OrderComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./form/home.component";
+import {FormsModule} from "@angular/forms";
+import { CustomerComponent } from './customer/customer.component';
+import { OrderComponent } from './order/order.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomerComponent,
+    OrderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
