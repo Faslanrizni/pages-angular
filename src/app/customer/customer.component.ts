1d7ce8b02899e7b726ac82c21d6d9ec1a45d2172
Faslan Rizni <Faslan2002Rizni@gmail.com>
2023-07-17 14:10:22 +0530

    data transfer from a page to another page

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.scss']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+  data:string | undefined;
+
+  constructor(private activatedRoute:ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    /*this.data =
+      this.activatedRoute.snapshot.paramMap.get('data')!;
+    /!* ! mark is for ensure to code that data would be string or undefined *!/*/
+     /* we are not using this mechanism when need real time update*/
+
+      this.activatedRoute.paramMap.subscribe(response =>{
+        this.data = response.get('data')!;
+      });
+  }
 
 }
